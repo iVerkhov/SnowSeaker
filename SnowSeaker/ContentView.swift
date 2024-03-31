b28536106b559a7e340dc4dea59e706a9be44f9f
Igor Verkhov <iVerkhov@icoud.com>
2024-03-31 11:23:51 +0300

    Initial Commit

---
+++ SnowSeaker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SnowSeaker
+//
+//  Created by Игорь Верхов on 31.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SnowSeaker/SnowSeakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeakerApp.swift
+//  SnowSeaker
+//
+//  Created by Игорь Верхов on 31.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
